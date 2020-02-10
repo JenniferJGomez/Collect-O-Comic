17fe8c36ef7b72917081849d442cddb8755de846
Jennifer Gomez <jennifergomezbruno@gmail.com>
2020-02-10 14:57:17 -0500

    back end

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
+
+    def show
+        user = User.find(params[:id])
+        render json: user.to_json(
+            :include => {:collections =>
+              {:except => [:created_at, :updated_at]}})
+    end
   
 end
